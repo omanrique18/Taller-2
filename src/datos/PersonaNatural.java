f25d26eae0b686e13eaaa01c6158ff7382205021
omanrique18 <51519737+omanrique18@users.noreply.github.com>
2019-06-11 21:48:43 -0500

    Update PersonaNatural.java

--- src/datos/PersonaNatural.java
+++ src/datos/PersonaNatural.java
@@ -1,5 +1,33 @@
 package datos;
 
-public class PersonaNatural {
+public class PersonaNatural extends Cliente{
+    private boolean prestamo;
+    private String tipoCliente;
     
-}
+	public boolean isPrestamo() {
+		return prestamo;
+	}
+	public void setPrestamo(boolean prestamo) {
+		this.prestamo = prestamo;
+	}
+	public String getTipoCliente() {
+		return tipoCliente;
+	}
+	public void setTipoCliente(String tipoCliente) {
+		this.tipoCliente = tipoCliente;
+	}
+    
+    public PersonaNatural (String tipoCliente) {
+    	setTipoCliente(tipoCliente);
+    	switch(tipoCliente) {
+    		case "ClienteA":
+    			setPrestamo(true);
+    			break;
+    		case "ClienteB":
+    			setPrestamo(true);
+    			break;
+    		case "ClienteC":
+    			setPrestamo(false);
+    			break;
+    	}
+    }
